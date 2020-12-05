ae583b33eb49aea9bf899f7d378573aa13262d44
Scott Bennett <scoyyz@gmail.com>
2020-12-05 18:14:45 -0500

    Added comments for Julia

--- Day5.py
+++ Day5.py
@@ -11,22 +11,22 @@ with open("Day5.txt") as file:
 
 
 for string in strings: 
-  print(string) 
-  valid = False
+  #print(string) 
+  valid = False #setting test value
   i = 0
-  while i < len(string)-1:
-    test = string[i:i+2]
+  while i < len(string)-1: #iterate through string
+    test = string[i:i+2] #sets test to equal a substring starting at index i and ending at index i+2
     print(test)
-    if re.search(test, string[i+2:]):
-      valid = True
+    if re.search(test, string[i+2:]): #searches for an instance of pattern "test" in the remainder of the string starting after index i+2
+      valid = True #if the regex search is successful, set valid to true
       break
     i += 1
   i = 0
-  if valid:
-    valid = False
-    while i < len(string)-2:
+  if valid: #confirming that first test passed
+    valid = False #resetting valid test to make sure both tests pass
+    while i < len(string)-2: #interate through string
       #print(string[i]+' '+string[i+1]+' '+string[i+2])
-      if (string[i] == string[i+2] and not string[i] == string[i+1]):
+      if (string[i] == string[i+2] and not string[i] == string[i+1]): #test if index i equals index i+2, while also not equaling i+1
         valid = True
         break
       i += 1
